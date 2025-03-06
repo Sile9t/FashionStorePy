33cee2d2db2a79e64418183f97e366fed97b5cdb
Sile9t <eyesless1jack@gmail.com>
2025-03-06 11:59:37 +0300

    UPDATE: added requried packages to local environment. FIX: changed launch config to run flaskr factory

--- back/app.py
+++ back/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flaskr import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
