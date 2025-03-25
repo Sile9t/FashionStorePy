87ac3cbb37ea607219ddb0cc5a1288f83a3a9da7
Sile9t <eyesless1jack@gmail.com>
2025-03-25 15:37:15 +0300

    FIX: removed auth service methods return types

--- src/fashionstore/service/authService.py
+++ src/fashionstore/service/authService.py
@@ -6,7 +6,7 @@ from dependency_injector.wiring import Provide
 from .baseService import BaseService
 
 class AuthService(BaseService):
-    async def getUser(self, id: int = 0) -> UserDto | None:
+    async def getUser(self, id: int = 0):
         userRecord = await UserDAO.find_one_or_none_by_id(
                 self.sessionManager.get_session,
                 id
@@ -18,20 +18,20 @@ class AuthService(BaseService):
         
         return None
 
-    async def getAllUsers(self) -> UserDto | None:
+    async def getAllUsers(self):
         userRecords = await UserDAO.find_all(self.sessionManager.get_session)
 
         users = UserDto.model_dump(userRecords)
 
         return users
 
-    async def registerUser(self, values: UserCreateDto) -> int:
+    async def registerUser(self, values: UserCreateDto):
         userRecord = await UserDAO.add(self.sessionManager.get_session, values)
         user = UserDto.model_validate(userRecord)
         
         return user.id
 
-    async def deleteUser(self, id: int) -> UserDto | None:
+    async def deleteUser(self, id: int):
         userRecord = await UserDAO.find_one_or_none_by_id(self.sessionManager.get_session, id)
         
         if userRecord:
